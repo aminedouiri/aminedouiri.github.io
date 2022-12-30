bbe45ac5e5b2019d14f3d3fefd1950a1df5d73d4
aminedouiri <aminedouiri2018@gmail.com>
2022-12-30 13:38:20 +0100

    update design and add scrollup btn

---
+++ js/scrollup.js
@@ -0,0 +1,28 @@
+
+const scrollTop = function () {
+    const scrollBtn = document.createElement("button");
+    scrollBtn.innerHTML = "&uarr;";
+    scrollBtn.setAttribute("id", "scroll-btn");
+    document.body.appendChild(scrollBtn);
+    console.log('function is up');
+  };
+
+scrollTop();
+
+const scrollBtnDisplay = function () {
+    window.scrollY > window.innerHeight
+      ? scrollBtn.classList.add("show")
+      : scrollBtn.classList.remove("show");
+};
+window.addEventListener("scroll", scrollBtnDisplay);
+
+const scrollWindow = function () {  
+    if (window.scrollY != 0) {
+      setTimeout(function () {
+        window.scrollTo(0, window.scrollY - 50);
+        scrollWindow();
+      }, 10);
+    }
+  };
+const scrollBtn = document.getElementById("scroll-btn");
+scrollBtn.addEventListener("click", scrollWindow);
