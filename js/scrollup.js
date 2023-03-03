2cd513202e68813445f740d394d3a1fe276832ae
Amine Douiri <aminedouiri2018@gmail.com>
2023-03-03 16:54:57 +0100

    Update scrollup.js

--- js/scrollup.js
+++ js/scrollup.js
@@ -4,22 +4,21 @@ const scrollTop = function () {
     scrollBtn.innerHTML = "&uarr;";
     scrollBtn.setAttribute("id", "scroll-btn");
     document.body.appendChild(scrollBtn);
-    console.log('function is up');
   };
 
 scrollTop();
 
 const scrollBtnDisplay = function () {
-    window.scrollY > window.innerHeight
+    window.scrollY
       ? scrollBtn.classList.add("show")
       : scrollBtn.classList.remove("show");
 };
 window.addEventListener("scroll", scrollBtnDisplay);
 
 const scrollWindow = function () {  
-    if (window.scrollY != 0) {
+    if (window.scrollY) {
       setTimeout(function () {
-        window.scrollTo(0, window.scrollY - 10);
+        window.scrollTo(0, window.scrollY - 50);
         scrollWindow();
       }, 10);
     }
